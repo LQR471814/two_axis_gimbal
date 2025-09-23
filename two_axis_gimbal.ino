88d59bc4f30a3aac583fd2ec58e83b9c661a42fb
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-09-23 09:20:35 -0700

    wip: add debugging

--- two_axis_gimbal.ino
+++ two_axis_gimbal.ino
@@ -3,6 +3,27 @@
 Servo servo;
 
 void setup(void) {
+  Serial.begin(9600);
+
+  // defaults should be:
+  // - phase correct PWM 8-bit
+  // - prescalar 64
+  // - TOP = 255
+  // - OCR1A = 0
+  // - OC1A disconnected
+  //
+  // duty cycle time (s): 2 * (TOP - OCR1A) * 64 / (16*10^6)
+  // total cycle time (s): 2 * OCR1A * 64 / (16*10^6)
+  //
+  // set COM1A0 = 1 (toggle mode)
+  // OCR1A = value from [0, 130]
+
+  Serial.print(TCCR1A);
+  Serial.print(" ");
+  Serial.print(TCCR1B);
+  Serial.print(" ");
+  Serial.print(TCCR1C);
+  Serial.print("\n");
   servo.setup();
 }
 
