be8a627deb08d9f1fdf1a4f73ee5168aa2f0b006
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-09-25 12:25:16 -0700

    fix: unchanging gyro values

--- gyro.ino
+++ gyro.ino
@@ -27,9 +27,8 @@ static constexpr int GYRO_REG = 0x43;
 static uint8_t write_reg(uint8_t reg, uint8_t value) {
     Wire.beginTransmission(MPU_ADDR);
     TRY(Wire.write(reg) == 0, return 1);
-    TRY(Wire.endTransmission(false), return err + 10);
     TRY(Wire.write(value) == 0, return 2);
-    TRY(Wire.endTransmission(true), return err + 20);
+    TRY(Wire.endTransmission(), return err + 10);
     return 0;
 }
 
@@ -44,25 +43,26 @@ namespace Gyro {
 
     // reg: PWR_MGMT_1
     // value: disables sleep mode, uses PLL with X axis gyro for clock
-    TRY(write_reg(0x6B, 0b00000001), return err + 20);
+    TRY(write_reg(0x6B, 0), return err + 10);
 
     // reg: PWR_MGMT_2
     // value: all disabled
-    TRY(write_reg(0x6C, 0), return err + 40);
+    TRY(write_reg(0x6C, 0), return err + 20);
 
     // reg: GYRO_CONFIG
     // enable self test and use FSR +-250
-    TRY(write_reg(0x1B, 0b11100000), return err + 60);
+    TRY(write_reg(0x1B, 0), return err + 30);
 
     // reg: ACCEL_CONFIG
     // enable self test and use FSR +-8g
-    TRY(write_reg(0x1C, 0b11100010), return err + 80);
+    TRY(write_reg(0x1C, 0b10), return err + 40);
 
     return 0;
   }
 
   uint8_t read(int (&gyro)[3], int (&accel)[3]) {
     // request registers from ACCEL_REG -> GYRO_REG from device
+    // this guarantees that all register data is from the same sample
     TRY(Wire.requestFrom(MPU_ADDR, 14, ACCEL_REG, 1, true) != 14, return err);
 
     char i = 0;
