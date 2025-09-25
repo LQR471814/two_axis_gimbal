ec10bd3806dfeaa1d9112eb380736cd72214043a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-09-25 11:48:55 -0700

    fix: gyro setup, errors with reading

--- gyro.ino
+++ gyro.ino
@@ -1,10 +1,15 @@
 #include "gyro.h"
+#include "constants.h"
 
 static constexpr int MPU_ADDR = 0x68;
-static constexpr int ACCEL_REG = 59;
-static constexpr int GYRO_REG = 67;
+static constexpr int ACCEL_REG = 0x3B;
+static constexpr int GYRO_REG = 0x43;
 
+#ifdef DEBUG
 #define TRY(expr, catch) { uint8_t err = expr; if (err != 0) { catch; } }
+#else
+#define TRY(expr, catch) expr;
+#endif
 
 /**
  * Notes on the Wire.h library.
@@ -19,47 +24,79 @@ static constexpr int GYRO_REG = 67;
  * and the things inside the buffer
  */
 
+static uint8_t write_reg(uint8_t reg, uint8_t value) {
+    Wire.beginTransmission(MPU_ADDR);
+    TRY(Wire.write(reg) == 0, return 1);
+    TRY(Wire.endTransmission(false), return err + 10);
+    TRY(Wire.write(value) == 0, return 2);
+    TRY(Wire.endTransmission(true), return err + 20);
+    return 0;
+}
+
 namespace Gyro {
   uint8_t setup(void) {
-    // internals:
-    // beginTransmission just sets a bunch of variables, it does not actually
-    // call any twi functions to transmit anything
-    Wire.beginTransmission(MPU_ADDR);
+    // reg: 0x6B
+    // value: DEVICE_RESET
+    TRY(write_reg(0x6B, 0b10000000), return err);
 
-    // PWR_MGMT_1 register
-    TRY(Wire.write(0x6B) != 0, return 1);
-    TRY(Wire.endTransmission(false), return err + 10);
+    // wait 100ms for reset to complete
+    delay(100);
 
-    // disables sleep mode, uses PLL with X axis gyro for clock
-    TRY(Wire.write(0b00000001) != 0, return 2);
-    TRY(Wire.endTransmission(true), return err + 20);
+    // reg: PWR_MGMT_1
+    // value: disables sleep mode, uses PLL with X axis gyro for clock
+    TRY(write_reg(0x6B, 0b00000001), return err + 20);
+
+    // reg: PWR_MGMT_2
+    // value: all disabled
+    TRY(write_reg(0x6C, 0), return err + 40);
+
+    // reg: GYRO_CONFIG
+    // enable self test and use FSR +-250
+    TRY(write_reg(0x1B, 0b11100000), return err + 60);
+
+    // reg: ACCEL_CONFIG
+    // enable self test and use FSR +-8g
+    TRY(write_reg(0x1C, 0b11100010), return err + 80);
 
     return 0;
   }
 
   uint8_t read(int (&gyro)[3], int (&accel)[3]) {
-    // request internal register ACCEL_REG from device
-    TRY(Wire.requestFrom(MPU_ADDR, 6, ACCEL_REG, 2, true), return err);
+    // request registers from ACCEL_REG -> GYRO_REG from device
+    TRY(Wire.requestFrom(MPU_ADDR, 14, ACCEL_REG, 1, true) != 14, return err);
 
     char i = 0;
-    while (Wire.available() && i < 3) {
+    while (i < 3) {
+      if (!Wire.available()) {
+        return 120;
+      }
       char upper = Wire.read();
       if (!Wire.available()) {
-        break;
+        return 120;
       }
       char lower = Wire.read();
       accel[i] = lower | (upper << 8);
       i++;
     }
 
-    // request internal register GYRO_REG from device
-    TRY(Wire.requestFrom(MPU_ADDR, 6, GYRO_REG, 2, true), return err);
+    // skip temp. data
+    if (!Wire.available()) {
+      return 130;
+    }
+    Wire.read();
+    if (!Wire.available()) {
+      return 130;
+    }
+    Wire.read();
 
     i = 0;
-    while (Wire.available() && i < 3) {
+    while (i < 3) {
+      if (!Wire.available()) {
+        return 140;
+      }
       char upper = Wire.read();
       if (!Wire.available()) {
-        break;
+        return 140;
       }
       char lower = Wire.read();
       gyro[i] = lower | (upper << 8);
--- two_axis_gimbal.ino
+++ two_axis_gimbal.ino
@@ -1,38 +1,77 @@
 #include <Servo.h>
 #include <Wire.h>
 #include "gyro.h"
+#include "constants.h"
 
 static Servo servo;
 
+#ifdef DEBUG
 #define PRINT_ERR(expr, str, exit) { uint8_t err = expr; if (err != 0) { Serial.print(str); Serial.print(" Err: "); Serial.print(err); Serial.print("\n"); exit; } }
+#else
+#define PRINT_ERR(expr, str, exit) expr;
+#endif
 
 static void setup(void) {
   Serial.begin(9600);
   Wire.begin();
   servo.attach(3);
 
+  Wire.setWireTimeout(0);
   PRINT_ERR(Gyro::setup(), "I2C setup failed!", return);
   Serial.println("I2C setup complete!");
 }
 
 // observations:
-//   o1: error code (1) - write() when buffer full
-//   o1: error code (2) - received nack on endTransmission()
+//   o1.1: no bytes are available() for reading, however reading does not fail
+//   o1.2: read can fail silently in cases where a timeout is exceeded
+//   o1.3: timeout should be disabled by default
+//   o1.4: read can read no data in cases where no data is indeed read
 //
-// what works:
+//   o2.1: green light on gyroscope when pressed against pins
+//   o2.2: green light does not light when board is obviously rotated around
 //
+//   o3.1: error code (2) - received NACK on endTransmission()
+//   o3.2: recv NACK status is read from TWSR
+//   o3.3: CASE 1: incorrect address
+//   o3.3.1: NACK is sent as a result of both addresses, NOT case 1
+//   o3.4: CASE 2: sleep mode
+//   o3.4.1: very unlikely sleep mode will prevent I2C from working
+//   o3.5: CASE 3: incorrect I2C impl. somewhere
+//   o3.5.1: CASE 3: very unlikely, implementation is widely used, board is
+//     also highly rated
+//   o3.6: CASE 4: VLOGIC pin sets logic levels, logic levels incorrect for some reason
+//   o3.6.1: VLOGIC is likely tied to VCC, so it is likely set correctly
+//   o3.7: CASE 5: power supply is incorrect
+//   o3: CONCLUSION: suboptimal physical contact was the root of the issue.
+//     (Make sure you pull the chip upwards so that the right side of the
+//     holes are touching the right side of the pins)
+//
+//   o4.1: error code (6) on requestFrom()
+//   o4.2: CONCLUSION: requestFrom() returns number of bytes read, not error
+//   code
+//
+//   o5.1: accel and gyro values not changing despite present acceleration and
+//     rotation
+//   o5.2: not the result of any settings in register PWR_MGMT_2
+//   o5.3: not the result of the choice of clock source
+//   o5.4: not the result of sending 8-bit addr. instead of 16-bit addr.
+//   (though this did make the numbers correct)
+//   o5.5: not the result of slow sampling
 
+// conjectures:
+//   c1: by observations in o1.2-4, no data is being read
+//
+//   c2: based on o2.1 and o2.2, the green light likely indicates that stable
+//       contact with MCU and power is possible
+//
+//   c3: NACK on write to poweron is preventing data from being read
 
-// what works:
-//   k1: no errors
-// problem: gyroscope values are not changing
-//   p1: gyroscope not on
 
 static void loop(void) {
   int gyro[3];
   int accel[3];
 
-  PRINT_ERR(Gyro::read(gyro, accel), "Gyro read failed!", return);
+  PRINT_ERR(Gyro::read(gyro, accel), "Gyro read failed!", delay(1000); return);
 
   Serial.print("GYRO: ");
   Serial.print(gyro[0]);
@@ -48,6 +87,6 @@ static void loop(void) {
   Serial.print(accel[2]);
   Serial.print("\n");
 
-  delay(1000);
+  delay(50);
 }
 
