dcd0702aab4b2ec1307cfd036ce9578bad0c0dfd
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-09-27 07:35:19 -0700

    fix: random corruption of gyro data

--- gyro.ino
+++ gyro.ino
@@ -1,84 +1,95 @@
+#include <Wire.h>
 #include "gyro.h"
 #include "constants.h"
 
 static constexpr uint8_t MPU_ADDR = 0x68;
-static constexpr uint8_t ACCEL_REG = 0x3B;
-static constexpr uint8_t GYRO_REG = 0x43;
 
 #ifdef DEBUG
-#define TRY(expr, catch) { uint8_t err = expr; if (err != 0) { catch; } }
+#define TRY(expr, catch) { \
+  uint8_t err = expr; \
+  if (err != 0) { catch; } \
+}
 #else
 #define TRY(expr, catch) expr;
 #endif
 
-static uint8_t write_reg(uint8_t reg, uint8_t value) {
-  Wire.beginTransmission(MPU_ADDR);
-  TRY(Wire.write(reg) == 0, return 1);
-  TRY(Wire.write(value) == 0, return 2);
-  TRY(Wire.endTransmission(), return err + 10);
-  return 0;
-}
-
 namespace Gyro {
+  static uint8_t write_reg(uint8_t reg, uint8_t value) {
+    Wire.beginTransmission(MPU_ADDR);
+    TRY(Wire.write(reg) == 0, return 1)
+    TRY(Wire.write(value) == 0, return 2)
+    TRY(Wire.endTransmission(), return err + 10)
+    return 0;
+  }
+
   uint8_t setup(void) {
     // reg: PWR_MGMT_1
     // value: trigger DEVICE_RESET
-    TRY(write_reg(0x6B, 1 << 7), return err);
+    TRY(write_reg(0x6B, 1 << 7), return err)
     delay(100);
 
     // reg: PWR_MGMT_1
     // value: disables sleep mode, uses PLL with X axis gyro for clock
-    TRY(write_reg(0x6B, 0x01), return err + 10);
+    TRY(write_reg(0x6B, 0x01), return err + 10)
 
     // reg: PWR_MGMT_2
     // value: all disabled
-    TRY(write_reg(0x6C, 0x00), return err + 20);
+    TRY(write_reg(0x6C, 0x00), return err + 20)
 
     // reg: GYRO_CONFIG
     // use range +-250
-    TRY(write_reg(0x1B, 0x00), return err + 30);
+    TRY(write_reg(0x1B, 0x00), return err + 30)
 
     // reg: ACCEL_CONFIG
     // use range +-8g
-    TRY(write_reg(0x1C, 0x02), return err + 40);
+    TRY(write_reg(0x1C, 0x02), return err + 40)
 
     return 0;
   }
 
-  static inline uint8_t read16(uint16_t* out) {
-    if (!Wire.available()) { return 1; }
-    char upper = Wire.read();
-    if (!Wire.available()) { return 2; }
-    char lower = Wire.read();
+  static uint8_t read8(uint8_t* out) {
+#ifdef DEBUG
+    if (!Wire.available()) {
+      return 1;
+    }
+#endif
+    *out = Wire.read();
+    return 0;
+  }
+
+  static uint8_t read16(int16_t* out) {
+    uint8_t upper, lower;
+    TRY(read8(&upper), return 1)
+    TRY(read8(&lower), return 2)
     *out = lower | (upper << 8);
     return 0;
   }
 
-  uint8_t read(uint16_t gyro[3], uint16_t accel[3]) {
-    // request registers from ACCEL_REG -> GYRO_REG from device
-    // this guarantees that all register data is from the same sample
-    TRY(Wire.requestFrom(MPU_ADDR, 14, ACCEL_REG, 1, true) != 14, return err);
+  uint8_t read(int16_t gyro[3], int16_t accel[3]) {
+    TRY(Wire.requestFrom(MPU_ADDR, 14, 0x3B, 1, true) != 14, return err)
 
     uint8_t err;
 
-    err = read16(&(accel[0]));
-    if (err != 0) { return 10 + err; }
-    err = read16(&(accel[1]));
-    if (err != 0) { return 20 + err; }
-    err = read16(&(accel[2]));
-    if (err != 0) { return 30 + err; }
+// retry if data corrupted
+#define READ16(out, errprefix) err = read16(&(out)); \
+    if (err != 0) { return errprefix + err; }
+
+    READ16(accel[0], 10)
+    READ16(accel[1], 20)
+    READ16(accel[2], 30)
 
     // skip temp. data
-    uint16_t temp;
+    int16_t temp;
     err = read16(&temp);
+#ifdef DEBUG
     if (err != 0) { return 40 + err; }
+#endif
 
-    err = read16(&(gyro[0]));
-    if (err != 0) { return 50 + err; }
-    err = read16(&(gyro[1]));
-    if (err != 0) { return 60 + err; }
-    err = read16(&(gyro[2]));
-    if (err != 0) { return 70 + err; }
+    READ16(gyro[0], 50)
+    READ16(gyro[1], 60)
+    READ16(gyro[2], 70)
+
+#undef READ16
 
     return 0;
   }
--- two_axis_gimbal.ino
+++ two_axis_gimbal.ino
@@ -2,11 +2,21 @@
 #include <Wire.h>
 #include "gyro.h"
 #include "constants.h"
+#include <math.h>
 
 static Servo servo;
 
 #ifdef DEBUG
-#define PRINT_ERR(expr, str, exit) { uint8_t err = expr; if (err != 0) { Serial.print(str); Serial.print(" Err: "); Serial.print(err); Serial.print("\n"); exit; } }
+#define PRINT_ERR(expr, str, exit) { \
+  uint8_t err = expr; \
+  if (err != 0) { \
+    Serial.print(str); \
+    Serial.print(" Err: "); \
+    Serial.print(err); \
+    Serial.print("\n"); \
+    exit; \
+  } \
+}
 #else
 #define PRINT_ERR(expr, str, exit) expr;
 #endif
@@ -16,34 +26,88 @@ static void reset_gyro() {
   Serial.println("I2C setup complete!");
 }
 
-static void setup(void) {
-  Serial.begin(9600);
-  Wire.begin();
-  servo.attach(3);
-
-  reset_gyro();
+static inline float next_angle(float dt, float angle_prev, float angle_change, float angle_measured) {
+  return 0.98 * (angle_prev + angle_change * dt) + 0.02 * angle_measured;
 }
 
-static void loop(void) {
-  uint16_t gyro[3];
-  uint16_t accel[3];
+unsigned long last = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.setWireTimeout();
+
+  servo.attach(3);
+  reset_gyro();
+
+  last = micros();
+}
+
+float angles[3] { 0, 0, 0 };
+float gyrof[3] { 0, 0, 0 };
+float accelf[3] { 0, 0, 0 };
+
+void loop() {
+  int16_t gyro[3];
+  int16_t accel[3];
 
   PRINT_ERR(Gyro::read(gyro, accel), "Gyro read failed!", delay(1000); reset_gyro(); return);
+  float dt = (micros() - last) / 1000000.0;
 
-  Serial.print("GYRO: ");
-  Serial.print(gyro[0]);
-  Serial.print(" ");
-  Serial.print(gyro[1]);
-  Serial.print(" ");
-  Serial.print(gyro[2]);
-  Serial.print(" | ACCEL: ");
-  Serial.print(accel[0]);
-  Serial.print(" ");
-  Serial.print(accel[1]);
-  Serial.print(" ");
-  Serial.print(accel[2]);
+  // 2^15 units / 250 degrees = 131.072  unit/deg
+  gyrof[0] = gyro[0] / 131.072;
+  gyrof[1] = gyro[1] / 131.072;
+  gyrof[2] = gyro[2] / 131.072;
+  // 2^15 units / 2 g = 16384 unit/g
+  accelf[0] = accel[0] / 16384.0;
+  accelf[1] = accel[1] / 16384.0;
+  accelf[2] = accel[2] / 16384.0;
+
+  // atan(X/Z) yields pitch (rotation about Y)
+  float measured_pitch = atan2(accelf[0], accelf[2]);
+
+  // atan(Y/Z) yields roll (rotation about X)
+  float measured_roll = atan2(accelf[1], accelf[2]);
+
+  angles[0] = next_angle(dt, angles[0], gyrof[0], measured_roll);
+  angles[1] = next_angle(dt, angles[1], gyrof[1], measured_pitch);
+  // yaw just ends up drifting
+  angles[2] = angles[2] + gyrof[2] * dt;
+
+  // Serial.print("| GYRO: ");
+  // Serial.print(gyro[0]);
+  // Serial.print(" ");
+  // Serial.print(gyro[1]);
+  // Serial.print(" ");
+  // Serial.print(gyro[2]);
+  // Serial.print(" | ACCEL: ");
+  // Serial.print(accel[0]);
+  // Serial.print(" ");
+  // Serial.print(accel[1]);
+  // Serial.print(" ");
+  // Serial.print(accel[2]);
+  // Serial.print("\n");
+
+  Serial.print(angles[0]);
+  Serial.print(",");
+  Serial.print(angles[1]);
+  Serial.print(",");
+  Serial.print(angles[2]);
+  Serial.print(",");
+  Serial.print(gyrof[0]);
+  Serial.print(",");
+  Serial.print(gyrof[1]);
+  Serial.print(",");
+  Serial.print(gyrof[2]);
+  Serial.print(",");
+  Serial.print(accelf[0]);
+  Serial.print(",");
+  Serial.print(accelf[1]);
+  Serial.print(",");
+  Serial.print(accelf[2]);
+  Serial.print(",");
   Serial.print("\n");
 
-  delay(50);
+  delay(20);
 }
 
